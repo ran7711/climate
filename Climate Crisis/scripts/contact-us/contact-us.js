eb1647d3fa1ac01afa21fd954735c0e0cae7b49e
RonenZubkov <ronenownz@gmail.com>
2020-02-15 19:36:06 +0200

    JS Assignments added

--- Climate Crisis/scripts/contact-us/contact-us.js
+++ Climate Crisis/scripts/contact-us/contact-us.js
@@ -5,20 +5,34 @@ $(function() {
     $("#contact_form").submit(function(event) {
         /* Preventing Page from Refreshing*/
         event.preventDefault();
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "form_handler.php",
-            data: $("contact_form").serialize(),
-            success: function() {
-                // callback code here
-                console.log('Success');
-            },
-            error: function (data) {
-                console.log('An error occurred.');
-                console.log(data);
-            }
-        });
-    })
+        var form = $("contact_form").serialize();
+        console.log(form);
+
+        // $.ajax({
+        //     type: "POST",
+        //     dataType: "json",
+        //     url: "form_handler.php",
+        //     data: $("contact_form").serialize(),
+        //     success: function() {
+        //         // callback code here
+        //         console.log('Success');
+        //     },
+        //     error: function (data) {
+        //         console.log('An error occurred.');
+        //         console.log(data);
+        //     }
+        // });
+    });
+
+
 });
 
+/* On Radio Button (Event) Select */
+function onRadioBtnC(){
+
+    if (document.getElementById("male").checked) {
+        document.getElementById("subject_input").selectedIndex = 2;
+    } else if (document.getElementById("female").checked){
+        document.getElementById("subject_input").selectedIndex = 1;
+    }
+}
