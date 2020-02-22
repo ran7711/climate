6b62c97afc480489686f76c44187ee83750f7ca5
RonenZubkov <ronenownz@gmail.com>
2020-02-23 00:41:37 +0200

    added JS Validation for Email in Contact Us Form

--- Climate Crisis/scripts/contact-us/contact-us.js
+++ Climate Crisis/scripts/contact-us/contact-us.js
@@ -7,27 +7,19 @@ $(function() {
         /* Preventing Page from Refreshing*/
         event.preventDefault();
 
+        /* Serializing form into one Object */
         var form = $( this ).serializeArray();
-        console.log($("contact_form"));
-        console.log(form);
 
-        // $.ajax({
-        //     type: "POST",
-        //     dataType: "json",
-        //     url: "form_handler.php",
-        //     data: $("contact_form").serialize(),
-        //     success: function() {
-        //         // callback code here
-        //         console.log('Success');
-        //     },
-        //     error: function (data) {
-        //         console.log('An error occurred.');
-        //         console.log(data);
-        //     }
-        // });
+        /* Looping threw each key in form */
+        for (var key of form) {
+            if(key.name === 'email'){
+                key.value = cleanEmail(key.value);
+            }
+        }
+
+        /* sending Ajax to a backend server */
+        sendForm(form);
     });
-
-
 });
 
 /* On Radio Button (Event) Select */
@@ -38,4 +30,36 @@ function onRadioBtnC(){
     } else if (document.getElementById("female").checked){
         document.getElementById("subject_input").selectedIndex = 1;
     }
+}
+
+function cleanEmail(email){
+
+    var notAllowedChars = ['!','#','$','%','^','&','*','(',')','=','+','[',']',';','\'','\\','"',':','/',',','javascript','script'];
+
+    for (var i = 0; i < email.length ; i++){
+        for (var j = 0; j < notAllowedChars.length ; j++){
+            if (email[i] === notAllowedChars[j]){
+                email = email.replace(notAllowedChars[j], '');
+            }
+        }
+    }
+    return email;
+}
+
+function sendForm(form){
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "form_handler.php",
+        data: form,
+        success: function() {
+            // callback code here
+            console.log('Success');
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            console.log(data);
+        }
+    });
 }
